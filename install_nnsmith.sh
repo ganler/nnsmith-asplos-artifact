1f683a6f1ce3ff137ff440cc936810310ae9fd75
ganler <jaway.liu@gmail.com>
2022-10-12 22:32:53 -0500

    fix: nnsmith_requirements.txt

--- install_nnsmith.sh
+++ install_nnsmith.sh
@@ -3,8 +3,9 @@
 set -e
 set -x
 
-python3 -m pip install -r nnsmith_requirements.txt
 cd "$(dirname "$0")" || exit 1
+
+python3 -m pip install -r "$(pwd)/nnsmith_requirements.txt"
 [ -d "$(pwd)/nnsmith" ] || git clone https://github.com/ise-uiuc/nnsmith.git
 cd nnsmith && git checkout 620645967a14d6a7b077cedd9c2c03ed74af50d9
 python3 -m pip install -r experiments/requirements.txt
